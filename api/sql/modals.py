78fa844cc9cfc656d3942e8b1b983e5463528668
Syed Faizan Ali Rehan <32485497+SyedFaizanAliRehan@users.noreply.github.com>
2024-01-19 10:45:31 +0530

    Updated modals
    Updated modals to sqlalchemy 2.0 syntax

--- api/sql/modals.py
+++ api/sql/modals.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column,Integer,String,DateTime
+from sqlalchemy import String,ForeignKey
+from sqlalchemy.orm import Mapped,mapped_column,relationship
 from database.connection import Base
 from enum import Enum
 from datetime import datetime
+from typing import List
 
 class User(Base):
-    __tablename__="users"
+    __tablename__ = "users"
     
     class UserFields(Enum):
         first_name = "First Name"
@@ -13,12 +15,13 @@ class User(Base):
         password = "Password"
         user_name = "User Name"
         
-    id:int = Column(Integer,primary_key=True,autoincrement=True,index=True)
-    user_name:str = Column(String,unique=True,index=True)
-    first_name:str = Column(String)
-    last_name:str = Column(String)
-    email:str = Column(String,unique=True)
-    password:str = Column(String)
+    id:Mapped[int] = mapped_column(primary_key=True,autoincrement=True,index=True)
+    user_name:Mapped[str] = mapped_column(String(10),unique=True,index=True)
+    first_name:Mapped[str] = mapped_column(String(10))
+    last_name:Mapped[str] = mapped_column(String(10))
+    email:Mapped[str] = mapped_column(String(25),unique=True,index=True)
+    password:Mapped[str] = mapped_column(String(15))
+    created_tasks:Mapped[List["Task"]] = relationship("Task",back_populates="created_by_user")
     
 
 class Task(Base):
@@ -34,16 +37,21 @@ class Task(Base):
         medium = "Medium"
         high = "High"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String, nullable=False)
-    description = Column(String[50])
-    status = Column(String, default=TaskStatus.not_started.value)
-    priority = Column(String, default=TaskPriority.low.value)
-    due_date = Column(DateTime)
-    assigned_to = Column(String)
+    id:Mapped[int] = mapped_column(primary_key=True,autoincrement=True,index=True)
+    title:Mapped[str] = mapped_column(String(30))
+    description:Mapped[str] = mapped_column(String(60))
+    status:Mapped[TaskStatus] = mapped_column(String(30),default=TaskStatus.not_started)
+    priority:Mapped[TaskPriority] = mapped_column(String(30),default=TaskPriority.low)
     
-    created_by = Column(String)
-    created_date = Column(DateTime, default=datetime.utcnow)
+    due_date:Mapped[datetime|None] = mapped_column(String(30),default=None)
+    assigned_to:Mapped[int] = mapped_column()
+    
+    created_by:Mapped[int] = mapped_column(ForeignKey("users.id"))
+    created_date:Mapped[datetime] = mapped_column(default=datetime.utcnow())
+    
+    created_by_user = relationship("User",back_populates="created_tasks")
+    
+    
     # last_modified_by = Column(String)
     # last_modified_date = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     
